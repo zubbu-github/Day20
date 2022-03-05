63a4d10e0dfa2bb8942ec9b581d0ea9c733ac473
LENOVO <mdzubeir2468@gmail.com>
2022-03-05 17:38:53 +0530

    Day 20 Stream API by anonymous ,consumer interface and lamda expressions

---
+++ Day20/src/day20/StreamAPI_Demo2.java
@@ -0,0 +1,31 @@
+package day20;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+//Consumer Interface 
+class A implements Consumer<Integer>{
+	 public void accept(Integer i) {
+		  System.out.println(i);
+	  }
+}
+public class StreamAPI_Demo2 {
+
+	public static void main(String[] args) {
+		List<Integer> values = Arrays.asList(1,2,3,4,5,6);
+	
+	values.forEach(i-> System.out.println(i));//Lamda Expression
+	
+	//Anonymous method
+		for(int i : values) {
+				values.forEach(new Consumer<Integer>(){
+			public void accept(Integer i) {
+				System.out.println(i);
+			}
+		});
+    }
+		Consumer<Integer> c = new  A();
+		values.forEach(c);
+}
+}
