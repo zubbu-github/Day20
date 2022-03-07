08d858be1618f0c292b42c18f7d45f00e93f82bd
LENOVO <mdzubeir2468@gmail.com>
2022-03-07 21:44:47 +0530

    day 20 double it

--- Day20/src/day20/StreamAPI_Demo2.java
+++ Day20/src/day20/StreamAPI_Demo2.java
@@ -15,10 +15,10 @@ public class StreamAPI_Demo2 {
 	public static void main(String[] args) {
 		List<Integer> values = Arrays.asList(1,2,3,4,5,6);
 	
-	values.forEach(i-> System.out.println(i));//Lamda Expression
+//	values.forEach(i-> System.out.println(i));//Lamda Expression
 	
 	//Anonymous method
-		for(int i : values) {
+	/*	for(int i : values) {
 				values.forEach(new Consumer<Integer>(){
 			public void accept(Integer i) {
 				System.out.println(i);
@@ -27,5 +27,11 @@ public class StreamAPI_Demo2 {
     }
 		Consumer<Integer> c = new  A();
 		values.forEach(c);
+		*/
+	values.forEach(StreamAPI_Demo2::doubleIt);
+	}
+
+       public static void doubleIt(int i) {
+	    System.out.println(i*2);
 }
 }
